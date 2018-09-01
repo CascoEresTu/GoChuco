ff912ae59217db8c90c82a2f0573efc9bee31620
CascoEresTu <k_k_ryuten@live.com>
2018-09-01 12:31:38 -0600

    Agregando files de los props

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Home.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(Home);
